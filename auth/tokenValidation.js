89ef511d96ff495bb95f0afb6114a40b43765b58
Naufal Akbar <kbrnugroho@gmail.com>
2022-05-12 07:56:24 +0700

    trying new token auth validation

--- auth/tokenValidation.js
+++ auth/tokenValidation.js
@@ -1,27 +1,53 @@
 const { verify } = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
 
-const checkToken = (req, res, next) => {
-    const token = req.get('authorization')
+const Admin = require('../api/admin/admin.model')
+
+const checkToken = asyncHandler(async (req, res, next) => {
+    let token
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1]
+
+            const decoded = verify(token, process.env.JWT_SECRET)
+
+            req.admin = await Admin.findById(decoded.id).select('-password')
+
+            next()
+        } catch (error) {
+            console.log(error)
+            res.status(401)
+            throw new Error('Not Authorized')
+        }
+    }
 
     if (!token) {
-        return res.status(401).json({
-            success: 0,
-            message: 'Access denied, no token provided'
-        })
-    } else {
-        let decoded = token.slice(7)
-        verify(decoded, process.env.JWT_SECRET, (err, decoded) => {
-            if (err) {
-                return res.status(401).json({
-                    success: 0,
-                    message: 'Invalid token'
-                })
-            } else {
-                let user = decoded.result
-                next()
-            }
-        })
+        res.status(401)
+        throw new Error('Not Authorized & No Token')
     }
-}
+
+    // const token = req.get('authorization')
+
+    // if (!token) {
+    //     return res.status(401).json({
+    //         success: 0,
+    //         message: 'Access denied, no token provided'
+    //     })
+    // } else {
+    //     let decoded = token.slice(7)
+    //     verify(decoded, process.env.JWT_SECRET, (err, decoded) => {
+    //         if (err) {
+    //             return res.status(401).json({
+    //                 success: 0,
+    //                 message: 'Invalid token'
+    //             })
+    //         } else {
+    //             let user = await decoded.result
+    //             next()
+    //         }
+    //     })
+    // }
+})
 
 module.exports = { checkToken }
