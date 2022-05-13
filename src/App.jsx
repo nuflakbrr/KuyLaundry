613e7f07f6114fc97740dcff9b5b47fed5922bfe
Naufal Akbar <kbrnugroho@gmail.com>
2022-05-13 19:35:33 +0700

    create developer endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import { Home, Login } from './pages'
+import { Developer, Home, Login } from './pages'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/developers" element={<Developer />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   )
