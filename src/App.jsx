21b258360d421c27b316ff68a68a6145f552612e
Naufal Akbar <kbrnugroho@gmail.com>
2022-05-14 17:37:18 +0700

    create route for admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import { Developer, Home, Login } from './pages'
+import { Dashboard, Member, Outlet, Package, Register, User } from './pages/admin'
 
 function App() {
   return (
@@ -9,6 +10,13 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/developers" element={<Developer />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/admin/dashboard" element={<Dashboard />} />
+      <Route path="/admin/outlet" element={<Outlet />} />
+      <Route path="/admin/package" element={<Package />} />
+      <Route path="/admin/member" element={<Member />} />
+      <Route path="/admin/transaction" element={<Member />} />
+      <Route path="/admin/user" element={<User />} />
+      <Route path="/admin/register" element={<Register />} />
     </Routes>
   )
 }
