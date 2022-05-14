08fa155997b3444599d244dfb96c90dd98b1c323
Naufal Akbar <kbrnugroho@gmail.com>
2022-05-14 23:09:57 +0700

    new route

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import { Developer, Home, Login, NotFound } from './pages'
-import { Dashboard, Member, Outlet, Package, Register, User } from './pages/admin'
+import { Dashboard, Member, Outlet, Package, Register, Transaction, User } from './pages/admin'
+
+// Outlet
+// import { AddForm } from './components/outlet'
 
 function App() {
   return (
@@ -12,9 +15,10 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/admin/dashboard" element={<Dashboard />} />
       <Route path="/admin/outlet" element={<Outlet />} />
+      {/* <Route path="/admin/outlet/add" element={<AddForm />} /> */}
       <Route path="/admin/package" element={<Package />} />
       <Route path="/admin/member" element={<Member />} />
-      <Route path="/admin/transaction" element={<Member />} />
+      <Route path="/admin/transaction" element={<Transaction />} />
       <Route path="/admin/user" element={<User />} />
       <Route path="/admin/register" element={<Register />} />
       <Route path="/*" element={<NotFound />} />
