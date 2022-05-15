8c6d3bbd81626b5d3acc338fca499269f95cb201
Naufal Akbar <kbrnugroho@gmail.com>
2022-05-15 18:08:24 +0700

    add route for edit admin

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import { Developer, Home, Login, NotFound } from './pages'
 import { Dashboard, Member, Outlet, Package, Transaction, User } from './pages/admin'
 
 // Outlet
-// import { AddForm } from './components/outlet'
+// import { AddOutlet } from './components/outlet'
 
 // User
-import { Register } from './components/user'
+import { EditAdmin, Register } from './components/user'
 
 function App() {
   return (
@@ -18,12 +18,13 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/admin/dashboard" element={<Dashboard />} />
       <Route path="/admin/outlet" element={<Outlet />} />
-      {/* <Route path="/admin/outlet/add" element={<AddForm />} /> */}
+      {/* <Route path="/admin/outlet/add" element={<AddOutlet />} /> */}
       <Route path="/admin/package" element={<Package />} />
       <Route path="/admin/member" element={<Member />} />
       <Route path="/admin/transaction" element={<Transaction />} />
       <Route path="/admin/user" element={<User />} />
       <Route path="/admin/user/add" element={<Register />} />
+      <Route path="/admin/user/edit/*" element={<EditAdmin />} />
       <Route path="/*" element={<NotFound />} />
     </Routes>
   )
