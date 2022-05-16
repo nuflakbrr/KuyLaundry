67e8c40956afbfbf2ba29838aa770e86589794f6
Naufal Akbar <kbrnugroho@gmail.com>
2022-05-16 19:03:37 +0700

    new route

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,35 @@ import { Dashboard, Member, Outlet, Package, Transaction, User } from './pages/a
 // Outlet
 // import { AddOutlet } from './components/outlet'
 
+// Package
+
+// Member
+import { AddMember, EditMember } from './components/member'
+
 // User
 import { EditAdmin, Register } from './components/user'
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/developers" element={<Developer />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/admin/dashboard" element={<Dashboard />} />
-      <Route path="/admin/outlet" element={<Outlet />} />
-      {/* <Route path="/admin/outlet/add" element={<AddOutlet />} /> */}
-      <Route path="/admin/package" element={<Package />} />
-      <Route path="/admin/member" element={<Member />} />
-      <Route path="/admin/transaction" element={<Transaction />} />
-      <Route path="/admin/user" element={<User />} />
-      <Route path="/admin/user/add" element={<Register />} />
-      <Route path="/admin/user/edit/*" element={<EditAdmin />} />
-      <Route path="/*" element={<NotFound />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/developers' element={<Developer />} />
+      <Route path='/login' element={<Login />} />
+
+      {/* Admin Side Route */}
+      <Route path='/admin/dashboard' element={<Dashboard />} />
+      <Route path='/admin/outlet' element={<Outlet />} />
+      {/* <Route path='/admin/outlet/add' element={<AddOutlet />} /> */}
+      <Route path='/admin/package' element={<Package />} />
+      <Route path='/admin/member' element={<Member />} />
+      <Route path='/admin/member/add' element={<AddMember />} />
+      <Route path='/admin/member/edit/*' element={<EditMember />} />
+      <Route path='/admin/transaction' element={<Transaction />} />
+      <Route path='/admin/user' element={<User />} />
+      <Route path='/admin/user/add' element={<Register />} />
+      <Route path='/admin/user/edit/*' element={<EditAdmin />} />
+      <Route path='/*' element={<NotFound />} />
     </Routes>
   )
 }
