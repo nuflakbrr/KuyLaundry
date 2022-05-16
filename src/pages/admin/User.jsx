d0cbabd82346d36f6e0c229372f814691fa9888d
Naufal Akbar <kbrnugroho@gmail.com>
2022-05-16 18:29:26 +0700

    fix

--- src/pages/admin/User.jsx
+++ src/pages/admin/User.jsx
@@ -94,7 +94,7 @@ export default function User() {
                                     </thead>
                                     <tbody>
                                         {
-                                            filteredResults.length === 0 && (
+                                            filteredResults.length && (
                                                 <tr>
                                                     <td colSpan="6" className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                                         <p className='text-gray-900 text-center'>
