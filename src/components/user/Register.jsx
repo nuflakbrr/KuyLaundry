1cc593da68e6141b75d378fd8cd733fc3ea28046
Naufal Akbar <kbrnugroho@gmail.com>
2022-05-16 18:47:30 +0700

    change `htmlFor` label email

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -72,7 +72,7 @@ export default function Register() {
                                 </select>
                             </div>
                             <div className='mt-4'>
-                                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='name'>
+                                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='email'>
                                     Email
                                 </label>
                                 <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' id='email' type='email' placeholder='Email' {...register('email', { required: true })} />
@@ -83,12 +83,6 @@ export default function Register() {
                                 </label>
                                 <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline' id='password' type='password' placeholder='******************' {...register('password', { required: true })} />
                             </div>
-                            {/* <div className='mt-4'>
-                                <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='password2'>
-                                    Confirm Password
-                                </label>
-                                <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline' id='password2' type='password' placeholder='******************' />
-                            </div> */}
                             <div className='mt-4'>
                                 <button className='bg-sky-500 hover:bg-sky-600 w-full text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type='submit'>
                                     Register
