073dc458f68dd2505b5fa63dc7e692dc38c18201
Naufal Akbar <kbrnugroho@gmail.com>
2022-05-16 22:13:12 +0700

    new route

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import { Developer, Home, Login, NotFound } from './pages'
 import { Dashboard, Member, Outlet, Package, Transaction, User } from './pages/admin'
 
 // Outlet
-// import { AddOutlet } from './components/outlet'
+import { AddOutlet, EditOutlet } from './components/outlet'
 
 // Package
+import { AddPackage, EditPackage } from './components/package'
 
 // Member
 import { AddMember, EditMember } from './components/member'
@@ -25,13 +26,21 @@ function App() {
 
       {/* Admin Side Route */}
       <Route path='/admin/dashboard' element={<Dashboard />} />
+
       <Route path='/admin/outlet' element={<Outlet />} />
-      {/* <Route path='/admin/outlet/add' element={<AddOutlet />} /> */}
+      <Route path='/admin/outlet/add' element={<AddOutlet />} />
+      <Route path='/admin/outlet/edit/*' element={<EditOutlet />} />
+
       <Route path='/admin/package' element={<Package />} />
+      <Route path='/admin/package/add' element={<AddPackage />} />
+      <Route path='/admin/package/edit/*' element={<EditPackage />} />
+
       <Route path='/admin/member' element={<Member />} />
       <Route path='/admin/member/add' element={<AddMember />} />
       <Route path='/admin/member/edit/*' element={<EditMember />} />
+
       <Route path='/admin/transaction' element={<Transaction />} />
+
       <Route path='/admin/user' element={<User />} />
       <Route path='/admin/user/add' element={<Register />} />
       <Route path='/admin/user/edit/*' element={<EditAdmin />} />
