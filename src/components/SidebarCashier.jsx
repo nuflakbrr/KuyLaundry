fbe7176cf981bdba105ee792a7f25bdf8f7c6ae2
Naufal Akbar <kbrnugroho@gmail.com>
2022-05-17 07:32:09 +0700

    fix link navigate

--- src/components/SidebarCashier.jsx
+++ src/components/SidebarCashier.jsx
@@ -46,7 +46,7 @@ export default function SidebarCashier() {
         <>
             <nav className='md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden bg-slate-800 flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6'>
                 <div className='md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto'>
-                    <NavLink className='md:block text-left md:pb-2 text-white mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0' to='/admin/dashboard'>
+                    <NavLink className='md:block text-left md:pb-2 text-white mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0' to='/cashier/dashboard'>
                         Halo, {cookie.admin.name}
                     </NavLink>
 
@@ -73,7 +73,7 @@ export default function SidebarCashier() {
                         <div className='md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200'>
                             <div className='flex flex-wrap'>
                                 <div className='w-6/12'>
-                                    <NavLink className='md:block text-left text-white md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm font-bold p-4 px-0' to='/admin/dashboard'>
+                                    <NavLink className='md:block text-left text-white md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm font-bold p-4 px-0' to='/cashier/dashboard'>
                                         <h1 className='text-white font-bold text-xl'><span className='text-sky-500'>Kuy</span>Laundry</h1>
                                     </NavLink>
                                 </div>
