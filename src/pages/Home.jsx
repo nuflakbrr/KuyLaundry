c8e31502bca53d7b3b103102413b3b1cf77fefb1
Naufal Akbar <kbrnugroho@gmail.com>
2022-06-29 15:48:18 +0700

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
                                     <path d='M22 11.08V12a10 10 0 11-5.93-9.14'></path>
                                     <path d='M22 4L12 14.01l-3-3'></path>
                                 </svg>
-                                <span className='title-font font-medium text-black'>Menggunakan Stack MERN (MongoDB, Express, React, Node)</span>
+                                <span className='title-font font-medium text-black'>Menggunakan Stack MERN (MongoDB, Express, React.js, Node.js)</span>
                             </div>
                         </div>
                         <div className='p-2 sm:w-1/2 w-full'>
